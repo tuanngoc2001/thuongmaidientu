6109691fd108d937da36519b6a937669d870f074
Nguyễn Tuấn Ngọc <72445778+tuanngoc2001@users.noreply.github.com>
2022-07-26 23:29:02 +0700

    thuongmaidientu

---
+++ src/main/java/com/example/thuongmaidientu/ThuongmaidientuApplication.java
@@ -0,0 +1,13 @@
+package com.example.thuongmaidientu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuongmaidientuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThuongmaidientuApplication.class, args);
+    }
+
+}
