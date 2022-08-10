df05e7f630ec0541d9ce9a4df746eac0272b7711
Văn cao <101122902+VanCao21@users.noreply.github.com>
2022-08-10 22:55:33 +0700

    done api pagging

--- src/main/java/com/example/thuongmaidientu/controller/ProductController.java
+++ src/main/java/com/example/thuongmaidientu/controller/ProductController.java
@@ -3,10 +3,13 @@ package com.example.thuongmaidientu.controller;
 import com.example.thuongmaidientu.model.Product;
 import com.example.thuongmaidientu.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.Optional;
 
@@ -30,6 +33,10 @@ public class ProductController {
     {
         return new ResponseEntity(productRepository.findAllByNameContaining(name), HttpStatus.OK);
     }
+@GetMapping("/phantrang-name")
+public ResponseEntity<Iterable<Product>> findAllByNameContaining(@RequestParam String name,@PageableDefault(size = 3) Pageable pageable) {
+    return new ResponseEntity<>(productRepository.findAllByNameContaining(pageable, name), HttpStatus.OK);
+}
 
     @PostMapping
     public ResponseEntity save(@RequestBody Product product) {
--- src/main/java/com/example/thuongmaidientu/repository/ProductRepository.java
+++ src/main/java/com/example/thuongmaidientu/repository/ProductRepository.java
@@ -1,12 +1,16 @@
 package com.example.thuongmaidientu.repository;
 
 import com.example.thuongmaidientu.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    List<Product> findAllByNameContaining(String name);
+    Page<Product> findAllByNameContaining(Pageable pageable, String name);
+    List<Product> findAllByNameContaining( String name);
 }
